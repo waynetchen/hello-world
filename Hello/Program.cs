05cda350a7bf48b5905372721385da7b8e6c7e2a
Wayne Chen <waynech@gmail.com>
2023-04-24 17:11:21 -0700

    Added Repeat Class

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!...this is me!");
+using System;
+
+
+
+class RepeatClass
+{
+
+    public int RepeatTimes = 10;
+
+    public void numberOfTimesToRepeat(int repeatTimes)
+    {
+        while (repeatTimes > 0)
+        {
+            Console.WriteLine("Count = {0}", repeatTimes);
+                        
+            repeatTimes--;
+        }
+    }
+
+
+    static void Main()
+    {
+        RepeatClass repeat = new RepeatClass();
+
+        repeat.numberOfTimesToRepeat(repeat.RepeatTimes);
+
+    }
+
+}
+
+
+    
+
